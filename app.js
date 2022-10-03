bdcd81eb9c43621cc5f124eefb6bdd28af6a13d6
Aniket Patel <ajp23695@gmail.com>
2022-10-03 15:02:38 +0100

    Salting and Hashing Applied on the Project

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const encrypt = require('mongoose-encryption');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRound = 12
 
 const app = express();
 app.set("view engine", "ejs");
@@ -46,34 +47,41 @@ app.get("/submit", function(req, res){
 
 //----------------------------All post routes-----------------------------------
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username ,
-    password: md5(req.body.password)
-  });
 
-  newUser.save(function(err){
-    if(err){
-      res.send(err);
-    } else {
-      res.render("secrets");
-    }
+  bcrypt.hash(req.body.password, saltRound, function(err, hash){
+    const newUser = new User({
+      email: req.body.username ,
+      password: hash
+    });
+
+    newUser.save(function(err){
+      if(err){
+        res.send(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 });
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email: username}, function(err, foundUser){
     if(err){
       res.send(err);
     } else {
       if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        } else {
-          res.send("Incorrect Login Details")
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result){
+          if(result === true){
+            res.render("secrets");
+          } else {
+            res.send("Incorrect Login Details")
+          }
+        });
+      } else {
+        res.send("User not found!!")
       }
     }
   });
